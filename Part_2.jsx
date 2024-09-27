e9c58b4c4c28b3c15bd1b63f0ebbb84bfbd67c9f
iqraashfaqiqra <112174330+iqraashfaqiqra@users.noreply.github.com>
2024-09-27 12:59:59 +0000

    a

---
+++ Part_2.jsx
@@ -0,0 +1,5 @@
+// FireBase
+// FireBase is online platform Where website base data,android data store.
+//open Firebase->go to console ->create project->
+//create project folder on computer->npm install --save firebase uuid
+//npmjs.com/packages/uuid
