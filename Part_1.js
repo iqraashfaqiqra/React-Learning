e49d28fb0c4676b51250d8efdca5997cd8eafd22
iqraashfaqiqra <112174330+iqraashfaqiqra@users.noreply.github.com>
2024-09-27 11:13:36 +0000

    a

--- Part_1.js
+++ Part_1.js
@@ -325,3 +325,164 @@ Function is stateless Component(if we want to use state then we use hook).
         }
         <input type="text" name="text" value={this.state.text} onChange={this.change}
 */
+// ..............................................
+/*
+How to Create a Registration Form using Greeting Screen
+index.js
+         <Registeration />
+Registeration.jsx
+            import React, { Component } from 'react'
+            import css from "./Registeration.module.css"
+            import Greeting from './Greeting';
+
+            export class Registeration extends Component { 
+            constructor(props) {
+                super(props)
+                this.props=props;
+                this.state={
+                name:"",
+                email:"",
+                password:"",
+                isrequired:false,
+                }
+            }
+            submit=(e)=>
+            {
+                alert("ge")
+                e.preventDefault();
+                const name=e.target.name.value;
+                const email=e.target.email.value;
+                const password=e.target.password.value;
+                this.setState({name,email,password,isrequired:true},()=>console.log(this.state))
+                }
+            
+            render() {
+                return (
+                <>
+                {this.state.isrequired?<Greeting/>:
+                    <div className={css.div}>
+                    <form onSubmit={this.submit}>
+                            <h1 className={css.h1}>Register</h1>
+                        <div>
+                        <label htmlFor="" className={css.red}>Name:</label>
+                        <input type="text" name="name" />
+                        </div>
+                        <div>
+                            <label htmlFor="">Email:</label>
+                            <input type="email" name="email"/>
+                        </div>
+                        <div>
+                            <label htmlFor="">Password:</label>
+                            <input type="password" name="password"/>
+                        </div>
+                        <div>
+                            <textarea name="" id=""  rows="10"></textarea>
+                        </div>
+                        <button>Submit</button>
+                    </form>
+                    </div>
+                }
+                </>
+                )
+                }
+            }
+            
+            export default Registeration
+ Greeting.jsx
+            import React, { Component } from 'react'
+            import Css from './Greeting.module.css'
+            export class Greeting extends Component {
+            render() {
+                return (
+                <div>
+                    <h1 className={Css.h1}>Thanks for Registeration</h1>
+                    <h1 className={Css.h3}>Congratulation!You have been registered.</h1>
+                </div>
+                )
+            }
+            }
+
+            export default Greeting
+*/
+// ..........................
+// Dynamic Css 
+/*
+         export default function App()
+         {
+            let cssClass=["btn",'mt-5'];
+            let [state,setState]=useState(false);
+            if(state)
+            {
+                cssClass.push("btn-danger");
+            }
+            else
+            {
+                cssClass.push("btn-primary");
+            }
+            return(
+                <>
+                <Button className={cssClass.join(" ")}>Show Password</button>  //Join Basic convert array to string
+                </>
+            )
+         }
+         */
+// ..................................
+// Styled Component 
+/*
+1-go to style component website
+ 2-npm install --save styled-components.
+ 3-import Styled from "styled-components"
+
+         const styledBtn=Styled.button `
+         display:block;
+         padding:10px solid red;
+         color:green;         
+         `
+        return(
+            <>
+             <styledBtn type="button">Login</styledBtn>
+            </>
+        )
+        
+ */
+// Hover & media Properties
+/*
+        const hoverProperty=Style.div`
+         &:hover {
+         color:red
+         }
+         @media (min-width:200px) and (max-width:600px)
+         {
+            width:50px;
+         }
+        `
+
+*/
+//Props in Error Function
+/*
+          const styledBtn=Styled.button `
+         display:block;
+         padding:10px solid &{props=>props.color};
+         color:green;         
+         `
+        return(
+            <>
+             <styledBtn type="button">Login</styledBtn>
+            </>
+        )    
+...................................
+Customized Button when flag 1 then 50% otherwsie 100%
+       const styledBtn=Styled.button `
+         display:&{props=>props.flag?"inline":"block"};
+         width:&{props=>props.flag?"50%":"100%"}
+         padding:10px solid &{props=>props.color};
+         color:green;         
+         `
+        return(
+            <>
+             <styledBtn flag="1" type="button">Login</styledBtn>
+             <styledBtn flag="1" type="button">Logout</styledBtn>
+             <styledBtn flag="0" type="button">Save</styledBtn>
+            </>
+        )    
+*/
