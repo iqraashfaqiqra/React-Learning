09ca8970b48a6446812447c2b9e956b692f872de
iqraashfaqiqra <112174330+iqraashfaqiqra@users.noreply.github.com>
2024-10-03 09:22:00 +0000

    a

--- Part_2.jsx
+++ Part_2.jsx
@@ -1,5 +1,29 @@
 // FireBase
 // FireBase is online platform Where website base data,android data store.
-//open Firebase->go to console ->create project->
-//create project folder on computer->npm install --save firebase uuid
-//npmjs.com/packages/uuid
+// ...............
+//Website
+/*
+.open Firebase->go to console ->create project-> </> ->Register App -> Copy Sdk.
+.create react project folder on computer->npm install firebase .
+.create Firebaseconfig.jsx and paste copy Sdk  and export app.
+.RealTime databas->copy URL
+.go to Paste Sdk in firebaseconfig.jsx and {databaseURL:"paste URL"}
+.index.jsx
+    import {getDatebase,ref,set} from 'firebase/database';
+    import app from './firebase'
+
+    const db=getDatabase(app);
+    const fun=()=>
+    {
+        set(ref(db,"user/push"),{
+        id:1,
+        name:"iqra",
+        age:20
+                                })
+    }
+   return(
+   <>
+   <button oncLick={fun}>Save</button>
+   </>
+   )
+*/
